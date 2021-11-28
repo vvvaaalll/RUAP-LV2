5346dea361b5ebb06609d890a6d1f9eba34d9b53
ValentinLoboda <vvvaaalll.98@gmail.com>
2021-11-28 18:40:20 +0100

    Exercise 3 Task 2 - Modifying the Index View to Provide a GUI for Creating Contacts

--- ruap-lv2/ruap-lv2/Controllers/ContactController.cs
+++ ruap-lv2/ruap-lv2/Controllers/ContactController.cs
@@ -24,5 +24,15 @@ namespace ruap_lv2.Controllers
         {
             return contactRepository.GetAllContacts();
         }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+
     }
 }
